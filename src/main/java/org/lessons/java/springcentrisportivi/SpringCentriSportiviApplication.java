2c212507e75b1d4e2e34387713fa3c5647ae5ad8
SofiaCurreri <sofiacurreri@gmail.com>
2023-10-18 11:44:40 +0200

    Init

---
+++ src/main/java/org/lessons/java/springcentrisportivi/SpringCentriSportiviApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.springcentrisportivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCentriSportiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCentriSportiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/springcentrisportivi/SpringCentriSportiviApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.springcentrisportivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCentriSportiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
