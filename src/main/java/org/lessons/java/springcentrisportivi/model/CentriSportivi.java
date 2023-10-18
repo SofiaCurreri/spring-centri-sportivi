84ea10ec47a39de086f8c321e3bf92d2d68c3506
SofiaCurreri <sofiacurreri@gmail.com>
2023-10-18 13:02:29 +0200

    'centri_sportivi' entity

---
+++ src/main/java/org/lessons/java/springcentrisportivi/model/CentriSportivi.java
@@ -0,0 +1,96 @@
+package org.lessons.java.springcentrisportivi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "centri_sportivi")
+public class CentriSportivi {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "E' necessario inserire un nome")
+    @Column(nullable = false)
+    private String nome;
+    private String descrizione;
+    @NotBlank(message = "E' necessario inserire un indirizzo")
+    @Column(unique = true, nullable = false)
+    private String indirizzo;
+    @NotBlank(message = "E' necessario inserire una città")
+    @Column(nullable = false)
+    private String citta;
+    @Column(nullable = false)
+    @NotNull(message = "E' necessario inserire una quota di iscrizione")
+    private Integer quotaIscrizione;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public Integer getQuotaIscrizione() {
+        return quotaIscrizione;
+    }
+
+    public void setQuotaIscrizione(Integer quotaIscrizione) {
+        this.quotaIscrizione = quotaIscrizione;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
