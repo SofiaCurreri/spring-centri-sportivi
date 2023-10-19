3e7cf83578e43dce7a858a7b4cc0e63cc99df064
SofiaCurreri <sofiacurreri@gmail.com>
2023-10-19 12:53:49 +0200

    CentroSportivo controller (index)

---
+++ src/main/java/org/lessons/java/springcentrisportivi/controller/CentroSportivoController.java
@@ -0,0 +1,29 @@
+package org.lessons.java.springcentrisportivi.controller;
+
+import org.lessons.java.springcentrisportivi.model.CentroSportivo;
+import org.lessons.java.springcentrisportivi.repository.CentroSportivoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/centri-sportivi")
+public class CentroSportivoController {
+
+    @Autowired
+    private CentroSportivoRepository centroSportivoRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<CentroSportivo> lista = centroSportivoRepository.findAll();
+        if (lista.isEmpty()) {
+            model.addAttribute("messaggio", "Non ci sono centri sportivi, mi dispiace :(");
+        }
+        model.addAttribute("lista", lista);
+        return "index";
+    }
+}
