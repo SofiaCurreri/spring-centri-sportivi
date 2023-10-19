8b53659eef0ce3af574bd473906e9a178faacaa2
SofiaCurreri <sofiacurreri@gmail.com>
2023-10-19 12:51:46 +0200

    CentroSportivoRepository

---
+++ src/main/java/org/lessons/java/springcentrisportivi/repository/CentroSportivoRepository.java
@@ -0,0 +1,7 @@
+package org.lessons.java.springcentrisportivi.repository;
+
+import org.lessons.java.springcentrisportivi.model.CentroSportivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CentroSportivoRepository extends JpaRepository<CentroSportivo, Integer> {
+}
