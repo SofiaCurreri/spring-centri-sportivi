e1777e53a799561f9ca78afaf198f410cd1892dc
SofiaCurreri <sofiacurreri@gmail.com>
2023-10-24 12:47:53 +0200

    added custom validation for unique constraint on 'nome'

--- src/main/java/org/lessons/java/springcentrisportivi/repository/SportRepository.java
+++ src/main/java/org/lessons/java/springcentrisportivi/repository/SportRepository.java
@@ -4,4 +4,7 @@ import org.lessons.java.springcentrisportivi.model.Sport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SportRepository extends JpaRepository<Sport, Integer> {
+
+    //per l' unicità del nome
+    boolean existsByNome(String nome);
 }
